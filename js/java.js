96b9144356d7830b98ee6191424438fa0f714f9e
SmithLam <lamphuoctuong@gmail.com>
2020-05-12 16:51:09 +0700

    Update the converter

--- js/java.js
+++ js/java.js
@@ -1,124 +1,50 @@
 
+//1. define variables
+let amount = document.getElementById("amount")
+let fromCurrency = document.getElementById("fromCurrency")
+let toCurrency = document.getElementById("toCurrency")
+let result = document.getElementById("result")
+let convertButton = document.getElementById("convertButton")
+let swapButton = document.getElementById("swapButton")
+
+
+//2. format Currency function
+function formatCurrency(type, value) {
+    const formatter = new Intl.NumberFormat(type, {
+      currency: type,
+      style: "currency"
+    });
+    return formatter.format(value);
+  }
+  
+//3. Currency Exchange rate
+const ExchangeRate = {
+      "usd": 1,
+      "gbp": 0.810996,
+      "vnd": 23235.5,
+      "inr": 75.5018,
+      "jpy": 107.541,
+      "dkr": 6.89021
+    }
+
+
+  
+//4. Convert currency function
 function convertCurrency(){
-// define result
-let text;
-
-//1. Define amount
-let amount = prompt("How much do you amount do you convert")
-
-//2. Define number
-let checkNumber = parseInt(amount)
-
-//2. Define From currency
-let fromCurrency = prompt("Which currency do you use")
-
-//3. Define To currency
-let toCurrency = prompt("Which currency do you to exchange")
-
-//4. Define currency ratio (23,161.39)
-const exchangeRateUSDandVND = 23161.39
-const exchangeRateUSDandGBP = 0.808591
-const exchangeRateGBPandVND = 28645.68
-
-//5. Define converting Functions
-
-//fromUSDtoVND
-function USDtoVND(){
-    //5-a. amount * currency ratio = result
-    //6. Define result
-    return (amount * exchangeRateUSDandVND)
+    let amountValue= amount.value;
+    let inputValue = document.getElementById("fromCurrency").value;
+    let outputValue = document.getElementById("toCurrency").value;
+    let convertedAmount = (amountValue *(ExchangeRate[outputValue] / ExchangeRate[inputValue])).toFixed(2)
+    let formatedInputAmount = formatCurrency(inputValue, amountValue)
+    let formatedOutputAmount = formatCurrency(outputValue,convertedAmount)
+    result.innerHTML=`Your ${formatedInputAmount} is ${formatedOutputAmount}`
 }
 
-//fromVNDtoUSD
-function VNDtoUSD(){
-    //5-a. amount * currency ratio = result
-    //6. Define result
-    return (amount / exchangeRateUSDandVND)
+//5. Swap function
+function swapCurrency(){
+    let inputValue = document.getElementById("fromCurrency").value;
+    let outputValue = document.getElementById("toCurrency").value;
+    document.getElementById("fromCurrency").value = outputValue; 
+    document.getElementById("toCurrency").value = inputValue;
+    return;
 }
-
-//fromUSDtoGBP
-function USDtoGBP(){
-    //5-a. amount * currency ratio = result
-    //6. Define result
-    return (amount * exchangeRateUSDandGBP)
-}
-
-//fromGBPtoUSD
-function GBPtoUSD(){
-    //5-a. amount * currency ratio = result
-    //6. Define result
-    return (amount / exchangeRateUSDandGBP)
-}
-
-function GBPtoVND(){
-    //5-a. amount * currency ratio = result
-    //6. Define result
-    return (amount * exchangeRateGBPandVND)
-}
-
-//fromVNDtoUSD
-function VNDtoGBP(){
-    //5-a. amount * currency ratio = result
-    //6. Define result
-    return (amount / exchangeRateGBPandVND)
-}
-
-
-
-//format-and-icon-for-currency
-function formatUSD(type, value) {
-    const formatter = new Intl.NumberFormat(type, {
-      currency: 'USD',
-      style: "currency"
-    });
-    return formatter.format(value);
-  }
-
-function formatVND(type, value) {
-    const formatter = new Intl.NumberFormat(type, {
-      currency: 'VND',
-      style: "currency"
-    });
-    return formatter.format(value);
-  }
-
-
-function formatGBP(type, value) {
-    const formatter = new Intl.NumberFormat(type, {
-      currency: 'GBP',
-      style: "currency"
-    });
-    return formatter.format(value);
-  }
-
-
-//if clause
-if (Object.is(checkNumber, NaN)){
-    text = "Please input a number"
-}
-else if (fromCurrency ==="USD" && toCurrency ==="VND"){
-    text =amount + " "+ fromCurrency + " in " + toCurrency + " is  " + formatUSD('en-US',USDtoVND(amount))
-}
-else if (fromCurrency ==="VND" && toCurrency ==="USD"){
-    text =amount + " "+ fromCurrency + " in " + toCurrency + " is " + formatVND('en-US',VNDtoUSD(amount))
-}
-else if (fromCurrency ==="USD" && toCurrency ==="GBP"){
-    text =amount + " "+ fromCurrency + " in " + toCurrency + " is " + formatGBP('en-GB',USDtoGBP(amount))
-}
-else if (fromCurrency ==="GBP" && toCurrency ==="USD"){
-    text =amount + " "+ fromCurrency + " in " + toCurrency + " is " + formatUSD('en-US',GBPtoUSD(amount))
-}
-else if (fromCurrency ==="GBP" && toCurrency ==="VND"){
-    text =amount + " "+ fromCurrency + " in " + toCurrency + " is " + formatVND('en-US',GBPtoVND(amount))
-}
-else if (fromCurrency ==="VND" && toCurrency ==="GBP"){
-    text =amount + " "+ fromCurrency + " in " + toCurrency + " is " + formatGBP('en-GB',VNDtoGBP(amount))
-}
-else {
-    text="This currency is not yet in our database!";
-}
-document.getElementById("result").innerHTML = text;
-}
-
-//(amount, fromCurrency, "to", toCurrency, "is", VNDtoUSD(amount), toCurrency)
-
